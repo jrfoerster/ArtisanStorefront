4222362355b53ec9bc4b5c1e9cfcf71c57a5a192
Justin Foerster <jrfoerster@gmail.com>
2021-08-31 20:45:28 -0400

    added Order and Product controllers

--- ArtisanStorefront.WebApi/Controllers/OrderController.cs
+++ ArtisanStorefront.WebApi/Controllers/OrderController.cs
@@ -7,32 +7,31 @@ using System.Web.Http;
 
 namespace ArtisanStorefront.WebApi.Controllers
 {
-    [Authorize]
-    public class ValuesController : ApiController
+    public class OrderController : ApiController
     {
-        // GET api/values
+        // GET: api/Order
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET: api/Order/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Order
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Order/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/Order/5
         public void Delete(int id)
         {
         }
