6571a775b3e4e44d76c32e6c8158ea0e4c6efc63
Justin Foerster <jrfoerster@gmail.com>
2021-08-31 20:56:33 -0400

    OrderService and ProductService

---
+++ ArtisanStorefront.Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisanStorefront.Services
+{
+    public class OrderService
+    {
+        private readonly Guid _userId;
+
+        public OrderService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+    }
+}
---
+++ ArtisanStorefront.Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisanStorefront.Services
+{
+    public class ProductService
+    {
+        private readonly Guid _userId;
+
+        public ProductService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+    }
+}
