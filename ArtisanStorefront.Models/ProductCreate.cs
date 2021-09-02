51ba671dd32c2b3bfa206c7d1088d83d3ec61523
Justin Foerster <jrfoerster@gmail.com>
2021-09-02 18:18:06 -0400

    Product Post

---
+++ ArtisanStorefront.Models/ProductCreate.cs
@@ -0,0 +1,28 @@
+﻿using ArtisanStorefront.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisanStorefront.Models
+{
+    public class ProductCreate
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        [Required]
+        public Category ProductType { get; set; }
+    }
+}
--- ArtisanStorefront.WebApi/Controllers/ProductController.cs
+++ ArtisanStorefront.WebApi/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ArtisanStorefront.Models;
+using ArtisanStorefront.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +13,12 @@ namespace ArtisanStorefront.WebApi.Controllers
     [Authorize]
     public class ProductController : ApiController
     {
+        private ProductService CreateProductService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            return new ProductService(userId);
+        }
+
         // GET: api/Product
         public IEnumerable<string> Get()
         {
@@ -23,8 +32,26 @@ namespace ArtisanStorefront.WebApi.Controllers
         }
 
         // POST: api/Product
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody] ProductCreate product)
         {
+            if (product is null)
+            {
+                return BadRequest("Http Request Body cannot be empty!");
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreateProductService();
+
+            if (service.CreateProduct(product) == false)
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
         }
 
         // PUT: api/Product/5
