6315f54978a731c853aad0eb140fd91c4e7b6c6d
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-09-02 14:17:53 -0400

    PUT / Order

---
+++ ArtisanStorefront.Models/EditOrder.cs
@@ -0,0 +1,25 @@
+﻿using ArtisanStorefront.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisanStorefront.Models
+{
+    public class EditOrder
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool IsExpedited { get; set; }
+    }
+}
--- ArtisanStorefront.Services/OrderService.cs
+++ ArtisanStorefront.Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArtisanStorefront.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,28 @@ namespace ArtisanStorefront.Services
         public OrderService(Guid userId)
         {
             _userId = userId;
+        }
+
+
+
+
+
+        //UPDATE
+        public bool UpdateNote(NoteEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                ctx
+                .Orders
+                .Single(e => e.OrderId == model.NoteId && e.OwnerId == _userId);
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
         }
 
 
