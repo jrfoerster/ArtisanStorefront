5b076916d4f5ca5a067838f88a94e057d3095dd6
Alexa Hurt <alexacorinnehurt@gmail.com>
2021-09-02 18:16:23 -0400

    ticket 9.1 and ticket 6.1

--- ArtisanStorefront.Services/OrderService.cs
+++ ArtisanStorefront.Services/OrderService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ArtisanStorefront.Data;
+using ArtisanStorefront.Models;
 
 namespace ArtisanStorefront.Services
 {
@@ -15,6 +17,49 @@ namespace ArtisanStorefront.Services
             _userId = userId;
         }
 
+        public OrderDetail GetOrderById(int orderId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Orders
+                        .Single(e => e.OrderId == orderId && e.SellerId == _userId);
+                return
+                    new OrderDetail
+                    {
+                        OrderId = entity.OrderId,
+                        Quantity = entity.Quantity,
+                        PurchaseDate = entity.PurchaseDate,
+                        AmountDue = entity.AmountDue,
+                        IsExpedited = entity.IsExpedited,
+                        BuyerId = entity.BuyerId,
+                        SellerId = entity.SellerId,
+
+                    };
+            }
+        }
+
+        public bool DeleteOrder(int orderId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Orders
+                        .Single(e => e.OrderId == orderId && e.SellerId == _userId);
+
+                ctx.Orders.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
 
     }
+
+   
+
+    
+
 }
--- ArtisanStorefront.WebApi/Controllers/OrderController.cs
+++ ArtisanStorefront.WebApi/Controllers/OrderController.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using ArtisanStorefront.Data;
 
 namespace ArtisanStorefront.WebApi.Controllers
 {
     public class OrderController : ApiController
     {
         // GET: api/Order
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        // public IEnumerable<string> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
 
         // GET: api/Order/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        // public string Get(int id)
+        // {
+        //    return "value";
+        // }
 
-        // POST: api/Order
-        public void Post([FromBody]string value)
+
+        //Get order by ID
+        [HttpGet]
+        public IHttpActionResult Get(int orderId)
+        {
+            OrderService orderService = CreateOrderService();
+            var order = orderService.GetOrderById(orderId);
+            return Ok(order);
+        }
+    }
+
+    // POST: api/Order
+    public void Post([FromBody]string value)
         {
         }
 
@@ -32,8 +45,18 @@ namespace ArtisanStorefront.WebApi.Controllers
         }
 
         // DELETE: api/Order/5
-        public void Delete(int id)
-        {
-        }
+      //  public void Delete(int id)
+       // {
+       // }
+
+    public IHttpActionResult Delete(int orderId)
+    {
+        var service = CreateOrderService();
+        if (!service.DeleteOrder(orderId))
+            return InternalServerError();
+
+        return Ok();
+
     }
+        
 }
